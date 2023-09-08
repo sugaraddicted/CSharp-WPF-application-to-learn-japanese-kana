3ef4ad744719bd1cfa22dc28036eb670a780d309
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-09-08 17:32:07 +0300

    added alphabet viewModel

---
+++ HiraganaApp/MVVM/ViewModel/AlphabetViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using GalaSoft.MvvmLight.Command;
+using HiraganaApp.MVVM.View;
+
+namespace HiraganaApp.MVVM.ViewModel
+{
+    public class AlphabetViewModel
+    {
+        public RelayCommand MenuButtonCommand { get; private set; }
+        public RelayCommand ExitButtonCommand { get; private set; }
+
+        public AlphabetViewModel()
+        {
+            MenuButtonCommand = new RelayCommand(NavigateToMainPage);
+            ExitButtonCommand = new RelayCommand(ExitApplication);
+        }
+
+        private void NavigateToMainPage()
+        {
+            if (Application.Current.MainWindow.FindName("MainFrame") is Frame frame)
+            {
+                frame.Navigate(new MainPage());
+            }
+        }
+
+        private void ExitApplication()
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
